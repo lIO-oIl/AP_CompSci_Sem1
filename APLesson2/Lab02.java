37fefa1cbb6a6f59df85ba1cdaf4ca327468e7da
ArQane <arqanegmaing@gmail.com>
2016-09-07 13:31:41 -0700

    Lab02

---
+++ APLesson2/Lab02.java
@@ -0,0 +1,24 @@
+public class Lab02
+{
+	public static void main(String[]args)
+	{
+		int num1 = 4;
+		int num2 = 5;
+		int result = num1 * num2;
+		
+		System.out.println(num1 + " multiplied with " + num2 + " is " + result);
+		
+		String name = "Me";
+		String Address = "1234 Example St";
+		String City = "San Diego";
+		String Zip = "92130";
+		
+		System.out.println("\n"+"Name: \t" + name + "\nAddress: " + Address + "\nCity: \t" + City + "\nZip: \t" + Zip);
+		
+		int length = 2;
+		int width = 3;
+		int height = 4;
+		
+		System.out.println("\n"+"The surface area of your rectangle is " + (2 * length * width) + (2 * height * length) + (2 * height * width);
+	}
+}
